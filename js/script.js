eb82483b27f77486798f517a4511ffb0f613cc90
adnanrahman0903 <adnanrahman8112001@gmail.com>
2023-06-10 12:47:43 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  items: 3,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
